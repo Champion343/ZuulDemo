7e3ba91b92deca1b555e7cd1d1beb4d67425f3a3
Huriel Hernandez <huriel.hernandez01@utrgv.edu>
2017-10-25 13:45:00 -0400

    Add @RefreshScope

--- src/main/java/com/example/demo/ZuulApplication.java
+++ src/main/java/com/example/demo/ZuulApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@RefreshScope
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulApplication {
